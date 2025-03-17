727bc3d2265d1302a25b6b9c1f141561d8379389
Yazeedx0 <yazeedkamel962@gmail.com>
2025-03-17 20:03:21 +0300

    init main

--- src/routes/base.py
+++ src/routes/base.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, APIRouter, Depends
 import os
 from helpers.config import get_settings, Settings
 
+
 base_router = APIRouter(
     prefix="/api/v1",
     tags=["api_v1"],
